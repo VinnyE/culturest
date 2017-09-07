d174f73061944706c3476c7fa7e9ed6e30a0165b
Vinny <espinalv1@gmail.com>
2017-09-07 06:16:03 -0400

    add jwt packages.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+var jwt = require('jsonwebtoken')
+var expressJwt = require('express-jwt')
 
 // router.get('/', function (req, res) {
 //   res.json({ message: 'Home request received!' })
 // })
 
 router.get('/twitter', function (req, res, next) {
-  console.log('step 1')
   next()
 }, passport.authenticate('twitter', {
   failureRedirect: '/',
@@ -17,9 +18,10 @@ router.get('/twitter', function (req, res, next) {
 router.get('/twitter/callback',
   passport.authenticate('twitter', { failureRedirect: '/', session: false }),
   function (req, res) {
-    // Successful authentication, redirect home.
-    console.log('step 2')
-    res.redirect('/')
+    const { id } = req.user.id
+    const token = jwt.sign({ id }, process.env.SESSION, { expiresIn: 60 * 5 })
+    res.cookie('token', JSON.stringify(token), { httpOnly: true })
+    res.redirect('/me')
   }
 )
 
