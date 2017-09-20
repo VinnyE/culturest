aba0777743c23fca3a3e14db4c9c2e9443739665
Vinny <espinalv1@gmail.com>
2017-09-20 05:31:42 -0400

    Fixed a bug where when adding a new pin, it would only appear after triggering a refresh.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 const authController = require('../controllers/authController')
 const pinController = require('../controllers/pinController')
-// authController.isAuthenticated, 
+
 router.get('/user/logout', userController.logout)
 
 router.get('/auth/twitter', authController.authUser)
